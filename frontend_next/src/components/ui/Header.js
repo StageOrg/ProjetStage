1171f842aca93396e1bed4a9e0cc8a259327eedc
thib228-dev <zthibaute2003@gmail.ccom>
2025-10-28 23:47:59 +0000

    minor change

--- frontend_next/src/components/ui/Header.js
+++ frontend_next/src/components/ui/Header.js
@@ -3,12 +3,8 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
 import AnneeAcademiqueService from "@/services/anneeAcademiqueService";
-=======
 import periodeInscriptionService from "@/services/inscription/periodeInscriptionService";
-
->>>>>>> feature/inscription-thib
 
 export default function Header() {
   const pathname = usePathname();
