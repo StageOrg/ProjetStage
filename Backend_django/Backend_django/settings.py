b6cb29c90d5fdbe4963478819ce2d85cf02827c1
lenaagbegninou <agblala02@gmail.com>
2025-12-05 01:14:46 +0000

    Fix backend settings

--- Backend_django/Backend_django/settings.py
+++ Backend_django/Backend_django/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 # Configuration CORS  sécurisée
@@ -63,6 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3001",  
+    "https://epl-pedago.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
