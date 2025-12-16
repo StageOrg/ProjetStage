62a6951cba30f4d5e6cf46d5622ee4d5cacd1ce3
thib228-dev <zthibaute2003@gmail.ccom>
2025-12-16 21:50:10 +0000

    modif

--- Backend_django/Backend_django/settings.py
+++ Backend_django/Backend_django/settings.py
@@ -100,9 +100,7 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default=5432, cast=int),
-        'OPTIONS': {
-        'sslmode': 'require',
-    }
+        
     }
 }
 # Media files
