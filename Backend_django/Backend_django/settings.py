a0b8f1d380af974e130f18caf8d56b2f6755c2b1
lenaagbegninou <agblala02@gmail.com>
2026-02-05 15:58:50 +0000

    cookies

--- Backend_django/Backend_django/settings.py
+++ Backend_django/Backend_django/settings.py
@@ -65,8 +65,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",  
     "https://epl.univ-lome.tg",
 ]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-USE_X_FORWARDED_HOST = True
 
 
 CORS_ALLOW_CREDENTIALS = True
